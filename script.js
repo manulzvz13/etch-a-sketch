c50a408c182baf0d63dc5d9e16c752957b294d1c
manulzvz13 <manulzvz@gmail.com>
2025-05-27 08:22:57 -0600

    feat: Add #container and .square-divs styles to style.css.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
+    div.classList.add("square-divs");
     container.appendChild(div);
   }
 }
