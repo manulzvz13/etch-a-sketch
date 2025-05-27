ef848fdf6a461a8e2a3b4fabf51c86d01fd848c9
manulzvz13 <manulzvz@gmail.com>
2025-05-27 16:08:35 -0600

    feat: Add addEventListener mouseover for the square divs

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(div);
   }
 }
+
+container.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseover target
+    event.target.style.background = "pink";
+    console.log(event.currentTarget);
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.background = "";
+    }, 1000);
+  },
+  false,
+);
+
+
