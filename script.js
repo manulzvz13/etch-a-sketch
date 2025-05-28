31c3e42dd1ee629216e4b0d742664e7f231b5c43
manulzvz13 <manulzvz@gmail.com>
2025-05-27 21:32:57 -0600

    feat: Try function adjustHeight

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#gridButton");
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
@@ -20,7 +21,25 @@ container.addEventListener(
       event.target.style.background = "";
     }, 1000);
   },
-  false,
+  false
 );
 
+gridButton.addEventListener("click", () => {
+  let squares = prompt("How many squares do you want?");
+  if (squares > 100) {
+    squares = prompt("Choose max 100 squares: ");
+  }
+  console.log(`Squares: ${squares}`);
+  let widthSquare = 1512 / squares - 4;
+  console.log(`widthSquare: ${widthSquare}`);
+  adjustHeight(widthSquare);
+});
 
+function adjustHeight(newHeight) {
+  // Set a new height (e.g., 200px)
+  // divs.style.height = `${newHeight}px`;
+  if (newHeight !== null) {
+    const divs = document.querySelectorAll(".square-divs");
+    divs.style.height = `${newHeight}px`;
+  }
+}
